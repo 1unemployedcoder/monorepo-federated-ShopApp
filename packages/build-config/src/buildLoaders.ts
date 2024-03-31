477dfa380976820071696a50a000bb3dd7d10c09
1unemployedcoder <psihbutgood@gmail.com>
2024-03-31 16:47:25 +0300

    remaked scss loader

--- packages/build-config/src/buildLoaders.ts
+++ packages/build-config/src/buildLoaders.ts
@@ -7,19 +7,12 @@ export function buildLoaders (options: BuildOptions): ModuleOptions['rules'] {
     const isDev = options.mode === 'development'
 
     const scssLoader = {
-        test: /\.s[ac]ss$/i,
+        test: /\.(sc|sa|c)ss$/i,
         use: [
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
             'css-loader',
+            'postcss-loader',
             'sass-loader'
-        ]
-    }
-
-    const cssLoader = {
-        test: /\.css$/i,
-        use: [
-            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-            'css-loader'
         ]
     }
 
@@ -52,5 +45,5 @@ export function buildLoaders (options: BuildOptions): ModuleOptions['rules'] {
             }
         ]
     }
-    return [scssLoader, babelLoader, assetLoader, svgLoader, cssLoader]
+    return [scssLoader, babelLoader, assetLoader, svgLoader]
 }
