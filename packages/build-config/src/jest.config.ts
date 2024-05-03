2b0bc4fbad4b74968bbb086a185e455246645dcd
1unemployedcoder <psihbutgood@gmail.com>
2024-05-03 20:17:58 +0300

    fixed css jest

--- packages/build-config/src/jest.config.ts
+++ packages/build-config/src/jest.config.ts
@@ -1,12 +1,13 @@
 import type {Config} from 'jest'
 
 export const JestConfig: Config = {
-    transform: {
-        "^.+\\.scss$": ["jest-transform-css", { modules: true }]
-    },
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
     moduleNameMapper: {
         '^@/(.*)$': '<rootDir>/src/$1',
-    }
+        '\\.(css|scss)$': 'identity-obj-proxy',
+    },
+    transform: {
+        '^.+\\.scss$': 'jest-transform-stub',
+    },
 };
