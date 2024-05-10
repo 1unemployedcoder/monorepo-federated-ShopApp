dbe02cc379467ff5aedae25d6f0443fae187b07f
1unemployedcoder <psihbutgood@gmail.com>
2024-05-10 04:29:58 +0300

    storybook

--- services/shop/src/stories/BtnOrdinary.stories.jsx
+++ services/shop/src/stories/BtnOrdinary.stories.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import BtnOrdinary from '@/components/ui/styledComponents/styledButton/BtnOrdinary'
+import IFButton from '@/components/ui/styledComponents/styledButton/IF_Button'
+import BtnPrimary from '@/components/ui/styledComponents/styledButton/BtnPrimary'
+import PropTypes from 'prop-types'
 export default {
     title: 'Components/BtnOrdinary',
-    component: BtnOrdinary
+    component: [BtnOrdinary, BtnPrimary, IFButton],
+    argTypes: {
+        usePrimary: {
+            control: 'boolean'
+        }
+    },
+    tags: ['autodocs']
 }
-export const Default = () => <BtnOrdinary>Default Button</BtnOrdinary>
+export const Ordinary = () => <BtnOrdinary>Ordinary</BtnOrdinary>
+export const Primary = () => <BtnPrimary>Primary</BtnPrimary>
+
+export const Optional = (props) => {
+    return <IFButton primary={props.usePrimary}>IfButton</IFButton>
+}
+Optional.propTypes = {
+    usePrimary: PropTypes.bool.isRequired
+}
+Optional.args = {
+    usePrimary: false
+}
